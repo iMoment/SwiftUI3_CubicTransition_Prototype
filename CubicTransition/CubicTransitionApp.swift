6655bbe56959d2c68a73a0261967332d18e5b4ee
Stanley Pan <imoment.developer@gmail.com>
2022-02-25 15:56:23 -0500

    Initial Commit

---
+++ CubicTransition/CubicTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubicTransitionApp.swift
+//  CubicTransition
+//
+//  Created by Stanley Pan on 2022/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CubicTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
