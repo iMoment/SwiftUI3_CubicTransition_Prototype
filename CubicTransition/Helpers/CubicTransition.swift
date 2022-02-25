1dc70653392dcf2a5c1e59265de158b37d356163
Stanley Pan <imoment.developer@gmail.com>
2022-02-25 16:21:57 -0500

    Added first part of animation with rotation3DEffect

--- CubicTransition/Helpers/CubicTransition.swift
+++ CubicTransition/Helpers/CubicTransition.swift
@@ -20,15 +20,37 @@ struct CubicTransition<Content: View, Detail: View>: View {
         self._show = show
     }
     
+    // MARK: Animation Properties
+    @State var animateView: Bool = false
+    @State var showView: Bool = false
+    
     var body: some View {
         GeometryReader { proxy in
             let size = proxy.size
+            
+            HStack(spacing: 0) {
+                content
+                    .frame(width: size.width, height: size.height)
+                
+                // Rotate current view when detail view is pushing
+                    .rotation3DEffect(.init(degrees: animateView ? -85 : 0), axis: (x: 0, y: 1, z: 0), anchor: .trailing, anchorZ: 0, perspective: 1)
+            }
+            // Apply offset
+            
+        }
+        .onChange(of: show) { newValue in
+            // Using separate variable instead of show since it will be removed as soon as it is set to false; animation will not be completed
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                withAnimation(.easeInOut(duration: 0.35)) {
+                    animateView.toggle()
+                }
+            }
         }
     }
 }
 
 struct CubicTransition_Previews: PreviewProvider {
     static var previews: some View {
-        CubicTransition()
+        ContentView()
     }
 }
--- CubicTransition/Views/HomeView.swift
+++ CubicTransition/Views/HomeView.swift
@@ -22,12 +22,41 @@ struct HomeView: View {
                         .aspectRatio(contentMode: .fill)
                         .frame(width: size.width, height: size.height)
                         .clipped()
+                    
+                    Button {
+                        show.toggle()
+                    } label: {
+                        Text("Navigate")
+                            .font(.title3)
+                            .foregroundColor(Color.white)
+                            .padding()
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(10)
+                            .environment(\.colorScheme, .dark)
+                    }
                 }
                 
             } detail: {
-                
+                Image("picture02")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: size.width, height: size.height)
+                    .clipped()
             }
 
+        }
+        .ignoresSafeArea()
+        .overlay(alignment: .top) {
+            HStack(spacing: 12) {
+                Text("Cubic Transition")
+                    .font(.title.bold())
+                    .foregroundColor(Color.white)
+            }
+            .padding()
+            .padding(.top, 4)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(.ultraThinMaterial)
+            .environment(\.colorScheme, .dark)
         }
     }
 }
